4b9b25e20452f58a011c34a8cd5aa733a72166b1
Luca Astolfi <316748@studenti.unimore.it>
2025-04-16 00:37:55 +0200

    W.I.P. test_all_problems + fixed MJ folder + removed base problem on main

--- src/main.py
+++ src/main.py
@@ -1,22 +1,17 @@
-from pymoo.visualization.scatter import Scatter
-from pymoo.algorithms.moo.nsga2 import NSGA2
-from pymoo.optimize import minimize
+import matplotlib
+matplotlib.use('Qt5Agg')  
+import matplotlib.pyplot as plt
+
 from pymoo.termination import get_termination
 from pymoo.problems import get_problem
 
-# --- Majority Judgment --- 
-from scripts.MJ.standard_majority_judgment import majority_judgment as standard_MJ
-
-# --- New Majority Judgment---
-from scripts.MJ.pile_majority_judgment import majority_judgment as pile_MJ
-from scripts.pymoo.mj_implementation.mj_survival import ParetoSurvival
 
 # --- Test PyMoo ---
-from test.test import get_phi_matrix_1, get_phi_matrix_2
-from test.pymoo.multi_obj_problem import MultiObjectiveProblem
+from test.test import *
 
 
 def get_results(problem, algorithms, termination, seed=1):
+    from pymoo.optimize import minimize
     results = {}
 
     for name, algorithm in algorithms.items():
@@ -29,21 +24,19 @@ def get_results(problem, algorithms, termination, seed=1):
 
     return results
     
-def print_results(results, title="default_title", scatter=True, radviz=True, save_file=False):
+def print_results(results, title="default_title", scatter=False, radviz=True, multi_problems=False, save_file=False):
     assert results, "Results Dict is Empty"
     
     assert (scatter or radviz), "It is necessary to choose a specific plot"
-
-    import matplotlib
-    matplotlib.use('Qt5Agg')  # Usa un backend interattivo
-    import matplotlib.pyplot as plt
-     
+    
     n_res = len(results)
 
     import colorsys
     colors = [colorsys.hsv_to_rgb(i / n_res, 1, 1) for i in range(n_res)]
 
     if scatter:
+        from pymoo.visualization.scatter import Scatter
+
         plot = Scatter(title=title, legend=True)
 
         for index, (name, result) in zip(range(n_res), results.items()):
@@ -52,7 +45,6 @@ def print_results(results, title="default_title", scatter=True, radviz=True, sav
         if save_file:
             plot.save("scatter")
         
-        plot.show()
         
 
     if radviz and n_res > 1:
@@ -65,23 +57,32 @@ def print_results(results, title="default_title", scatter=True, radviz=True, sav
         for res_F in results.values():
             F_normalized.append(normalize(res_F.F, xl=F_normalizer.F.min(axis=0), xu=F_normalizer.F.max(axis=0)))
 
-
         plot = Radviz(title=title, legend=True)
+
         for index, name, F_norm in zip(range(n_res), results.keys(), F_normalized):
             plot.add(F_norm, label=name, color=colors[index])
-
+        
         if save_file:
             plot.save("radvis")
-        
+
+    plt.tight_layout()
+
+    if not multi_problems:
         plot.show()
 
-# W.I.P.
-def test_problems(algorithms = [], problem_names = [], n_objs = [], n_gens = []):
+def test_all_problems(algorithms = {}, problem_names = [], n_objs = [], n_gens = [], scatter=False, radviz=True, save_file=False):
     assert algorithms, "Missing algorithms"
-    assert n_gens, "Missing number of generations"
     assert problem_names, "Missing problem names"
-    assert n_obj, "Missing number of objective function"
+    assert n_objs, "Missing number of objective function"
+    assert n_gens, "Missing number of generations"
     
+    fig, axes = plt.subplots(len(problem_names), len(n_objs), figsize=(5 * len(n_objs), 5 * len(problem_names)))
+
+    # Se c'è solo un'asse, metti in lista
+    if len(problem_names) == 1:
+        axes = [axes]
+
+    all_results = []
     for problem_name in problem_names:
         for n_obj in n_objs:
             for n_gen in n_gens:
@@ -91,46 +92,42 @@ def test_problems(algorithms = [], problem_names = [], n_objs = [], n_gens = [])
                 
                 results = get_results(problem, algorithms, termination)
 
-                print_results(results, title=f"{problem_name}, {n_obj}, {n_gen}")
+                print_results(results, title=f"{problem_name}, {n_obj}, {n_gen}", scatter=scatter, radviz=radviz, save_file=save_file)
+
+    plt.show()
 
 
 if __name__ == "__main__":
+    from pymoo.algorithms.moo.nsga2 import NSGA2
+    from pymoo.algorithms.moo.nsga3 import NSGA3
+    from pymoo.algorithms.moo.rvea import RVEA
+    from pymoo.util.ref_dirs import get_reference_directions
+    from scripts.pymoo.mj_survival import MJ_Survival
+
     pop_size = 100
-    n_gen = 200
-    problem_name = "dtlz1"
-
-    problem = get_problem(problem_name, n_obj=5)
     
-    algorithm_pile_MJ = NSGA2(
-        pop_size=pop_size,
-        survival=ParetoSurvival(use_MJ_pile=True)
-    )
-
-    algorithm_standard_MJ = NSGA2(
-        pop_size=pop_size,
-        survival=ParetoSurvival(use_MJ_pile=False)
-    )
-    
-    algorithm_NSGA2 = NSGA2(
-        pop_size=pop_size
-    )
+    ref_dirs = get_reference_directions("das-dennis", n_dim=3, n_partitions=12)
 
     algorithms = {
-        "pile_MJ": algorithm_pile_MJ,
-        "standard_MJ": algorithm_standard_MJ,
-        "NSGA2": algorithm_NSGA2
+        "pile_MJ": NSGA2(pop_size=pop_size, survival=MJ_Survival(use_MJ_pile=True)),
+        "standard_MJ": NSGA2(pop_size=pop_size, survival=MJ_Survival(use_MJ_pile=False)),
+        "NSGA2": NSGA2(pop_size=pop_size),
+        # "nsga3": NSGA3(pop_size=pop_size, ref_dirs=ref_dirs),
+        # "rvea": RVEA(pop_size=pop_size, ref_dirs=ref_dirs)
     }
 
-    termination = get_termination("n_gen", n_gen)
-
-    results = get_results(problem, algorithms, termination)
-
-    print_results(results, title=problem_name, scatter=False)
-    #test_problems([50, 200], )
-
+    #n_gen = 200
+    #problem_name = "dtlz1"
+    #problem = get_problem(problem_name, n_obj=5)
+    # termination = get_termination("n_gen", n_gen)
+    # results = get_results(problem, algorithms, termination)
+    # print_results(results, title=problem_name, scatter=False)
     
+    problems = [f"dtlz{i}" for i in range(1, 8)]
+    n_objs = [3, 5, 9]
+    n_gens = [50, 200, 400]
 
-
+    test_all_problems(algorithms, problems, n_objs, n_gens, radviz=True)
 
 
     
